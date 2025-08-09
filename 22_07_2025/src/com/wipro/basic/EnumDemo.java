734b311ae791f219a2cffff9baad69dae6e59b82
Supriya <shintresupriya6@gmial.com>
2025-08-09 09:38:10 +0530

    Add work for 22_07_2025

---
+++ 22_07_2025/src/com/wipro/basic/EnumDemo.java
@@ -0,0 +1,17 @@
+package com.wipro.basic;
+
+public class EnumDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] days= {"Sun","Mon","Tues"};
+		String day="Sun";
+		if(day.equalsIgnoreCase(days[0])) {
+			System.out.println("Sunday");
+			
+		}
+		
+
+	}
+
+}
---
+++ 22_07_2025/src/com/wipro/basic/FormData2.java
@@ -0,0 +1,32 @@
+package com.wipro.basic;
+import java.time.LocalDate;
+
+public class FormData2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Supriya Shintre";
+		String productNumber = "AR-5381";
+		String model = "First1";  
+		String category = "";
+		String subcategory = "";
+		String productLine = "";
+		String productClass = "";
+		String style = "";
+		String color = "";
+		int size = 15;  
+		double weight = 0.0;
+		double listPrice = 0.00;
+		double standardCost = 0.00;
+		LocalDate.parse("2025-07-22");
+		LocalDate.parse("2025-07-22");
+		LocalDate discountDate = null;      
+		int safetyStock = 1000;
+		int reorderPoint = 750;
+		int daysToManufacture = 50;
+		String make = "";
+		boolean finishedGoods = true;  
+
+	}
+
+}
