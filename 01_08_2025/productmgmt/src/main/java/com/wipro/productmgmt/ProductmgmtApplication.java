447f5d47aa148a126b95a5d8ab0bac82290f6844
Supriya <shintresupriya6@gmial.com>
2025-08-09 13:54:58 +0530

    Add productmgmt project under 01_08_2025

---
+++ 01_08_2025/productmgmt/src/main/java/com/wipro/productmgmt/ProductmgmtApplication.java
@@ -0,0 +1,12 @@
+package com.wipro.productmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductmgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductmgmtApplication.class, args);
+	}
+}
